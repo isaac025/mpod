7aae7ea903e9ce9c59e96cce0cc69015246b4e17
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2024-12-02 06:53:20 -0500

    repl and list tracks

--- main.c
+++ main.c
@@ -2,6 +2,22 @@
 #include <gpod/itdb.h>
 #include <stdlib.h>
 #include <glib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+/* list_tracks: list all tracks on ipod */
+void list_tracks(Itdb_iTunesDB* itdb)
+{
+    GList* tracks = itdb->tracks;
+    int count = 1;
+    for(GList* iter = tracks; iter != NULL; iter = iter->next) {
+        Itdb_Track* track = (Itdb_Track *)iter->data;
+        char* track_title = track->title ? track->title : "Unknown Title";
+        char* track_artist = track->artist ? track->artist : "Unknown Artist";
+        printf("%d. %s - %s\n", count, track_title, track_artist);
+        count++;
+    }
+}
 
 /* help: print all commands with description 
  * @cmd if a command name is provided print
@@ -9,7 +25,7 @@
 */
 void help(char* cmd)
 {
-    if(cmd) {
+/*    if(cmd) {
         switch(cmd) {
             case "help":
                 printf("help - print help menu\n");
@@ -29,33 +45,43 @@ void help(char* cmd)
         printf("help - print help menu\n");
         printf("list - list all tracks of ipod\n");
         printf("quit - quit mipod\n");
+    }*/
+    printf("help - print help menu\n");
+    printf("list - list all tracks of ipod\n");
+    printf("quit - quit mipod\n");
+}
+
+int parse_cmd(char* cmd, Itdb_iTunesDB* itdb)
+{
+    if(strcmp(cmd, "help") == 0) {
+        help(NULL);
+        return 1;
+    }
+    else if(strcmp(cmd, "list") == 0) {
+        list_tracks(itdb);
+        return 1;
+    }
+
+    else if(strcmp(cmd, "quit") == 0) {
+        return 0;
+    }
+    else {
+        printf("Command %s not found\n", cmd);
+        return 1;
     }
 }
 
 /* repl: read eval print loop function */
-int repl()
+int repl(Itdb_iTunesDB* itdb)
 {
-
-
-    return 0;
-}
-
-/* list_tracks: list all tracks on ipod */
-void list_tracks(Itdb_iTunesDB* itdb)
-{
-    if(!itdb) {
-        fprintf(stderr, "Failed to load iPod db from %s\n", ipod_path);
+    char* input = readline("mpod> ");
+    if(parse_cmd(input, itdb)) {
+        free(input);
         return 1;
     }
-
-    GList* tracks = itdb->tracks;
-    int count = 1;
-    for(GList* iter = tracks; iter != NULL; iter = iter->next) {
-        Itdb_Track* track = (Itdb_Track *)iter->data;
-        char* track_title = track->title ? track->title : "Unknown Title";
-        char* track_artist = track->artist ? track->artist : "Unknown Artist";
-        printf("%d. %s - %s\n", count, track_title, track_artist);
-        count++;
+    else {
+        free(input);
+        return 0;
     }
 }
 
@@ -68,9 +94,13 @@ int main(int argc, char* argv[])
     GError** error = NULL;
     Itdb_iTunesDB* itdb = itdb_parse(ipod_path, error);
 
-    while(1) {
-
+    if(!itdb) {
+        fprintf(stderr, "Failed to load iPod db from %s\n", ipod_path);
+        return 1;
     }
+
+    int repl_result = 1;
+    while(repl_result = repl(itdb)); 
 
     itdb_free(itdb);
     return 0;
